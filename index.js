a0f55ba1d0c54a8aae2953f82168e16842f28d48
Ronopoldo [Рони] <>
2021-11-05 22:10:11 +0000

    Динамичная таблица вместо сообщений.

--- index.js
+++ index.js
@@ -12,62 +12,78 @@ app.listen(port, () => console.log());
 const Discord = require("discord.js")
 const client = new Discord.Client()
 
+try{
+    client.on('message', msg => {
+      if (msg.content == '/тест')
+      {
+        msg.channel.send('Ы')
+
+      }
+    })
+
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
-  const alertChannel = client.channels.cache.get('897177098026905730') //АЙДИ КАНАЛА ОПОВЕЩЕНИЙ
-
+  const alertChannel = client.channels.cache.get('841694881688322138') //АЙДИ КАНАЛА ОПОВЕЩЕНИЙ
   const guild = client.guilds.cache.get('544902879534907392')
-// let user = client.users.cache.get("54490218007813652");
 
-// console.log(user.username)
-    const statusMessage = new Discord.MessageEmbed()
-.setTitle("КРИТИЧЕСКАЯ ОШИБКА!!!!! ЭТОТ БОТ БЫЛ В ОТКЛЮЧКЕ!!!!")
-.setColor("#ffff00")
-.setDescription("ВНИМАНИЕ! Этот бот был отключён!!! Если Вы считаете, что этого не должно было быть, то СРОЧНО сообщите в ЛС <@544902183007813652>")
-
-.setFooter("Ronoserver Services - звено Дебага", "https://media.discordapp.net/attachments/768414683019345931/841704850139906108/9b6a4cc843e31c1e.png")
-.setThumbnail("https://media.discordapp.net/attachments/698853696817070164/896895090121924638/d9e8efab4c698503.png?width=779&height=670")
-
-    alertChannel.send(statusMessage)
+    //alertChannel.send(statusMessage)
 let RonobotLastStatus = true
 let ZelenskyLastStatus = true
 let CyalmLastStatus = true
 let DJLastStatus = true
+let RSLastStatus = true
 
-console.log('123')
+let RonobotLastStatus1 = false
+let ZelenskyLastStatus1 = false
+let CyalmLastStatus1 = false
+let DJLastStatus1 = false
+let RSLastStatus1 = false
+
+
+let RonobotAnswer = '✅Активен✅'
+let ZelenskyAnswer = '✅Активен✅'
+let CyalmAnswer = '✅Активен✅'
+let RonoRatingAnswer = '🏗Бета версия🏗'
+let DJAnswer = '✅Активен✅'
+let RSManagerAnswer = '✅Активен✅'
+let DownDetectorAnswer = '✅Активен✅'
+let SanitarAnswer = '📡Больше не работает📡'
+
+let StatusCountMSG = '✅На данный момент сбоев не наблюдается✅'
+let StatusColor = '#22fa05'
+
+let RonobotCounter = 0
+let DownCount = 0
 
   var interval = setInterval (function () {
 
       guild.members.fetch('664263594002612263').then((member) => {
       console.log(member.user.username);
       console.log(member.user.presence.status);
+
+
+    RonobotLastStatus1 = RonobotLastStatus
+    ZelenskyLastStatus1 = ZelenskyLastStatus
+    CyalmLastStatus1 = CyalmLastStatus
+    DJLastStatus1 = DJLastStatus
+    RSLastStatus1 = RSLastStatus
+            
+
+
       if ((member.user.presence.status == 'offline') && RonobotLastStatus == true)
       {
-            const statusMessage = new Discord.MessageEmbed()
-              .setTitle("Сбой в работе Ронобота!")
-              .setColor("#ff0000")
-              .setDescription("Сервер <@664263594002612263> перестал отвечать и его необхоимо перезапустить! <@544902183007813652> сделает это как можно скорее!")
-
-              .setFooter("Ronoserver Services - звено Дебага", "https://media.discordapp.net/attachments/768414683019345931/841704850139906108/9b6a4cc843e31c1e.png")
-              .setThumbnail("https://media.discordapp.net/attachments/698853696817070164/896884943299416085/181b7f3638aa25d7.png?width=277&height=669")
-
-            alertChannel.send(statusMessage)
+            RonobotAnswer = '❌Сбой❌'
             RonobotLastStatus = false
+            RonobotCounter = 0
+            DownCount = DownCount + 1
       }
 
       if ((member.user.presence.status !== 'offline') && RonobotLastStatus == false)
       {
-            const statusMessage = new Discord.MessageEmbed()
-              .setTitle("Работа Ронобота восстановлена!")
-              .setColor("#00ff00")
-              .setDescription("<@664263594002612263> вернулся в штатный режим работы!")
-
-              .setFooter("Ronoserver Services - звено Дебага", "https://media.discordapp.net/attachments/768414683019345931/841704850139906108/9b6a4cc843e31c1e.png")
-              .setThumbnail("https://media.discordapp.net/attachments/698853696817070164/896884865822261308/e3c03ddae2f4d506.png?width=667&height=669")
-
-            alertChannel.send(statusMessage)
             RonobotLastStatus = true
+            RonobotCounter = 0
+            DownCount = DownCount - 1
       }
     })
 
@@ -79,30 +95,16 @@ console.log('123')
       console.log(member.user.presence.status);
       if ((member.user.presence.status == 'offline') && ZelenskyLastStatus == true)
       {
-            const statusMessage = new Discord.MessageEmbed()
-              .setTitle("Сбой в работе Зеленского!")
-              .setColor("#ff0000")
-              .setDescription("Сервер <@852202671443804160> перестал отвечать и его необхоимо перезапустить! <@544902183007813652> сделает это как можно скорее!")
-
-              .setFooter("Ronoserver Services - звено Дебага", "https://media.discordapp.net/attachments/768414683019345931/841704850139906108/9b6a4cc843e31c1e.png")
-              .setThumbnail("https://media.discordapp.net/attachments/698853696817070164/896884943299416085/181b7f3638aa25d7.png?width=277&height=669")
-
-            alertChannel.send(statusMessage)
+            ZelenskyAnswer = '❌Сбой❌'
             ZelenskyLastStatus = false
+            DownCount = DownCount + 1
       }
 
       if ((member.user.presence.status !== 'offline') && ZelenskyLastStatus == false)
       {
-            const statusMessage = new Discord.MessageEmbed()
-              .setTitle("Работа Зеленского восстановлена!")
-              .setColor("#00ff00")
-              .setDescription("<@852202671443804160> вернулся в штатный режим работы!")
-
-              .setFooter("Ronoserver Services - звено Дебага", "https://media.discordapp.net/attachments/768414683019345931/841704850139906108/9b6a4cc843e31c1e.png")
-              .setThumbnail("https://media.discordapp.net/attachments/698853696817070164/896884865822261308/e3c03ddae2f4d506.png?width=667&height=669")
-
-            alertChannel.send(statusMessage)
+            ZelenskyAnswer = '✅Активен✅'
             ZelenskyLastStatus = true
+            DownCount = DownCount - 1
       }
     })
 
@@ -110,34 +112,18 @@ console.log('123')
 ////////////////////////
 
     guild.members.fetch('775337700428742707').then((member) => {
-      console.log(member.user.username);
-      console.log(member.user.presence.status);
-      if ((member.user.presence.status == 'offline') && CyalmLastStatus == true)
-      {
-            const statusMessage = new Discord.MessageEmbed()
-              .setTitle("Сбой в работе Циалма!")
-              .setColor("#ff0000")
-              .setDescription("Сервер <@775337700428742707> перестал отвечать и его необхоимо перезапустить! <@544902183007813652> сделает это как можно скорее!")
+                      if ((member.user.presence.status == 'offline') && CyalmLastStatus == true)
+                      {
 
-              .setFooter("Ronoserver Services - звено Дебага", "https://media.discordapp.net/attachments/768414683019345931/841704850139906108/9b6a4cc843e31c1e.png")
-              .setThumbnail("https://media.discordapp.net/attachments/698853696817070164/896884943299416085/181b7f3638aa25d7.png?width=277&height=669")
-
-            alertChannel.send(statusMessage)
+            CyalmAnswer = '❌Сбой❌'
             CyalmLastStatus = false
-      }
-
+            DownCount = DownCount + 1
+                      }
       if ((member.user.presence.status !== 'offline') && CyalmLastStatus == false)
       {
-            const statusMessage = new Discord.MessageEmbed()
-              .setTitle("Работа Циалма восстановлена!")
-              .setColor("#00ff00")
-              .setDescription("<@775337700428742707> вернулся в штатный режим работы!")
-
-              .setFooter("Ronoserver Services - звено Дебага", "https://media.discordapp.net/attachments/768414683019345931/841704850139906108/9b6a4cc843e31c1e.png")
-              .setThumbnail("https://media.discordapp.net/attachments/698853696817070164/896884865822261308/e3c03ddae2f4d506.png?width=667&height=669")
-
-            alertChannel.send(statusMessage)
+            CyalmAnswer = '✅Активен✅'
             CyalmLastStatus = true
+            DownCount = DownCount - 1
       }
     })
 
@@ -146,40 +132,91 @@ console.log('123')
 /////////////////
 
     guild.members.fetch('887823801298088006').then((member) => {
-      console.log(member.user.username);
-      console.log(member.user.presence.status);
-      if ((member.user.presence.status == 'offline') && DJLastStatus == true)
-      {
-            const statusMessage = new Discord.MessageEmbed()
-              .setTitle("Сбой в работе ДиДжея!")
-              .setColor("#ff0000")
-              .setDescription("Сервер <@887823801298088006> перестал отвечать и его необхоимо перезапустить! <@544902183007813652> сделает это как можно скорее!")
-
-              .setFooter("Ronoserver Services - звено Дебага", "https://media.discordapp.net/attachments/768414683019345931/841704850139906108/9b6a4cc843e31c1e.png")
-              .setThumbnail("https://media.discordapp.net/attachments/698853696817070164/896884943299416085/181b7f3638aa25d7.png?width=277&height=669")
-
-            alertChannel.send(statusMessage)
+                if ((member.user.presence.status == 'offline') && DJLastStatus == true)
+                {
+            DJAnswer = '❌Сбой❌'
             DJLastStatus = false
-      }
-
+            DownCount = DownCount + 1
+                }
       if ((member.user.presence.status !== 'offline') && DJLastStatus == false)
       {
-            const statusMessage = new Discord.MessageEmbed()
-              .setTitle("Работа ДиДжея восстановлена!")
-              .setColor("#00ff00")
-              .setDescription("<@887823801298088006> вернулся в штатный режим работы!")
-
-              .setFooter("Ronoserver Services - звено Дебага", "https://media.discordapp.net/attachments/768414683019345931/841704850139906108/9b6a4cc843e31c1e.png")
-              .setThumbnail("https://media.discordapp.net/attachments/698853696817070164/896884865822261308/e3c03ddae2f4d506.png?width=667&height=669")
-
-            alertChannel.send(statusMessage)
+            DJAnswer = '✅Активен✅'
             DJLastStatus = true
+            DownCount = DownCount - 1
       }
     })
+
+///////////////
+        guild.members.fetch('711014269180706827').then((member) => {
+                if ((member.user.presence.status == 'offline') && RSLastStatus == true)
+                {
+            RSManagerAnswer = '❌Сбой❌'
+            RSLastStatus = false
+            DownCount = DownCount + 1
+
+            console.log('ЫГГЫЫГЫ')
+                }
+      if ((member.user.presence.status != 'offline') && DJLastStatus == false)
+      {
+
+            RSManagerAnswer = '✅Активен✅'
+            RSLastStatus = true
+            DownCount = DownCount - 1
+      }
+    })
+
+    if ((RSLastStatus1 != RSLastStatus) || (DJLastStatus1 !=DJLastStatus) || (ZelenskyLastStatus1 != ZelenskyLastStatus) || (RonobotLastStatus1 != RonobotLastStatus) || (CyalmLastStatus1 != CyalmLastStatus))
+    {
+
+          if (DownCount == 0) {StatusCountMSG = '✅На данный момент сбоев не наблюдается✅'
+          StatusColor = '#22fa05'}
+          if (DownCount == 1) {StatusCountMSG = '⚡Обнаружен сбой в работе одного сервиса⚡'
+          StatusColor = '#a8fe00'}
+          if (DownCount == 2) {StatusCountMSG = '⚠Обнаружен сбой в работе двух сервисов⚠'
+          StatusColor = '#f5fd00'}
+          if (DownCount == 3) {StatusCountMSG = '⛔Обнаружен сбой в работе трёх сервисов⛔'
+          StatusColor = '#fe0d01'}
+          if (DownCount == 4) {StatusCountMSG = '🔥Обнаружен сбой в работе четырёх сервисов🔥'
+          StatusColor = '#b902fd'}
+          if (DownCount == 5) {StatusCountMSG = '🪦Обнаружен сбой в работе всех сервисов (гг)🪦'
+          StatusColor = '#000000'}
+
+
+          const status = new Discord.MessageEmbed()
+          .setTitle("Статусы Ботов")
+          .setColor(StatusColor)
+          .setDescription("Здесь Вы найдёте полный список ботов сервера, сделанных для него и статус каждого из них!\nИнформация обновляется каждые 30 секунд.\n\n" + StatusCountMSG + '\n\n\n**RB ' + RonobotAnswer + ' - __<@664263594002612263>__**\n' + '**ZL  ' + ZelenskyAnswer + ' - __<@852202671443804160>__**\n' + '**CY ' + CyalmAnswer + ' - __<@775337700428742707>__**\n' + '**RT ' + RonoRatingAnswer + ' - __<@899380887282675743>__**\n' + '**DJ  ' + DJAnswer + ' - __<@887823801298088006>__**\n' + '**RS  ' + RSManagerAnswer + ' - __<@711014269180706827>__**\n' + '**DT ' + DownDetectorAnswer + ' - __<@896799577053749258>__**\n' + '**ST ' + SanitarAnswer + ' - __<@692507306986045460>__**\n')
+
+          .setFooter("Ronoserver Services - звено Дебага", "https://media.discordapp.net/attachments/768414683019345931/841704850139906108/9b6a4cc843e31c1e.png")
+          // .setThumbnail("https://media.discordapp.net/attachments/768414683019345931/841705746857197628/hourglass-clipart-hour-18.png?width=669&height=669")
+
+
+// let RonobotAnswer = '✅Активен✅'
+// let ZelenskyAnswer = '✅Активен✅'
+// let CyalmAnswer = '✅Активен✅'
+// let RonoRatingAnswer = '🏗Бета версия🏗'
+// let DJAnswer = '✅Активен✅'
+// let RSManagerAnswer = '✅Активен✅'
+// let DownDetectorAnswer = '✅Активен✅'
+// let SanitarAnswer = '📡Больше не работает📡'
+
+alertChannel.messages.fetch('906286753583882302').then(msg => {msg.edit('Монитор активен.') //
+msg.edit(status)})
+    }
+    console.log(DownCount)
+              console.log('RS' + RSLastStatus1 + RSLastStatus)
+
+          console.log('DJ' + DJLastStatus1 + DJLastStatus)
+          console.log('ZL' + ZelenskyLastStatus1 + ZelenskyLastStatus)
+          console.log('RB' + RonobotLastStatus1 + RonobotLastStatus)
+          console.log('CA' + CyalmLastStatus1 + CyalmLastStatus)
+
     }, 30000);
+})
 
 
 
+}catch(err){console.log(err)}
 
 
-});client.login(process.env.TOKEN) // НЕ ТРОГАТЬ,Ондрей: мне то зачем это трогать?
+client.login(process.env.TOKEN) // НЕ ТРОГАТЬ,Ондрей: мне то зачем это трогать?
